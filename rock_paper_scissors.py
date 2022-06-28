86645a80af391190de01ddfa12cb215592959540
JanainaRoriz <janainalimar@gmail.com>
2022-06-28 16:51:27 -0300

    código ainda precisando de uns ajustes antes de ser finalizado

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -6,4 +6,23 @@ player = False
 while player == False:
     player(input("rock, paper or scissors? "))
 if player == computer:
-print("It's a tie!")
+    print("It's a tie!")
+elif player == 'rock':
+    if computer == 'paper':
+        print('You lose!', computer, 'covers', player)
+    else:
+        print('You win!', player, 'smashes', computer)
+elif player == 'paper':
+    if computer == 'scissors':
+        print('You lose!', computer, 'cuts', player)
+    else:
+        print('You win!', player, 'covers', computer)
+elif player == 'scissors':
+    if computer == 'rock':
+        print('You lose!', computer, 'smashes', player)
+    else:
+        print('You win!', player, 'cut', computer)
+else:
+    print('Check out your answer! You must have written it wrong.')
+player = False
+computer = game[randint(0,2)]
